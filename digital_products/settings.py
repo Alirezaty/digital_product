30ddadc1213908ab03eda8ec24ba32655b44ff8c
Alirezaty <alirezataaty02@gmail.com>
2023-03-27 22:59:53 +0430

    users admin and models added.

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -126,3 +126,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#autorization
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,143 @@
-from django.db import models
+import random
 
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, send_mail
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, phone_number, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not username:
+            raise ValueError('the given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(phone_number=phone_number,
+                          username=username,email=email,
+                          is_staff=is_staff,is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+        if not extra_fields.get('no_password'):
+            user.set_password(password)
+
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username=None,phone_number=None, email=None, password=None, **extra_fields):
+        if username is None:
+            if email:
+                username = email.split('@', 1)[0]
+            if phone_number:
+                username = random.choice('abcdefghijklmnopqrstuvwxyz') + str(phone_number)[-7:]
+            while User.objects.filter(usename=username).exists():
+                username += str(random.randint(10,99))
+
+        return self._create_user(username,phone_number, email, password, False, False, **extra_fields)
+
+    def create_superuser(self,username,phone_number, email, password, **extra_fields):
+        return self._create_user(username,phone_number, email, password,True, True, **extra_fields)
+
+    def get_by_phone_number(self, phone_number):
+        return self.get(**{'phone_number': phone_number})
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=32, unique=True,
+                                 help_text=_('Required. 30 characters or fewer starting with a letter'),
+                                 validators=[
+                                     validators.RegexValidator(r'^[a-zA-Z][a-zA-Z0-9_\.]+$',
+                                                               _('Enter a valid username'), 'invalid'),
+                                 ],
+                                 error_messages={
+                                     'unique' : _("A user with that username already exist."),
+                                 }
+                                 )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), unique=True, null=True, blank=True)
+    phone_number = models.BigIntegerField(_('mobile number'), unique=True, null=True, blank=True,
+                                          validators=[
+                                              validators.RegexValidator(r'^989[0-3,9]\d{8}$',
+                                                                                _('Enter a valid mobile number.'))
+                                                      ],
+                                          error_messages= {
+                                              'unique' : _('A user with this mobile number already exist.'),
+                                          }
+                                          )
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+                                   help_text=_('Designates whether this user should be treated as active.'
+                                               'Unselect this instead of deleting account'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    last_seen = models.DateTimeField(_('last seen date'), null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'phone_number']
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nick_name = models.CharField(_('nick_name'), max_length=150, blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True)
+    birthday = models.DateField(_('birthday'), null=True, blank=True)
+    gender = models.BooleanField(_('gender'), help_text=_('female is False, male is True, null is unset'), null=True)
+    province = models.ForeignKey(verbose_name=_('province'), to='Province', null=True, on_delete=models.SET_NULL)
+    # email = models.EmailField(_('email address'), blank=True)
+    # phone_number = models.BigIntegerField(_('mobile number'), null=True, blank=True,
+    #                                       validators=[validators.RegexValidator(r'^989[0-3,9]\d{8}$',
+    #                                                                             _('Enter a valid mobile number.'))
+    #                                                   ],
+    #                                       )
+    class Meta:
+        db_table = 'user_profiles'
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+
+
+class Device(models.Model):
+    WEB = 1
+    IOS = 2
+    ANDROID = 3
+    DEVICE_TYPE_CHOICES = (
+        (WEB,_('web')),
+        (IOS,_('ios')),
+        (ANDROID,_('android'))
+    )
+
+    user = models.ForeignKey(User, related_name='devices', on_delete=models.CASCADE)
+    device_uuid = models.UUIDField(_('device uuid'), null=True)
+
+    last_login = models.DateTimeField(_('last login date'), null=True)
+    device_type = models.PositiveSmallIntegerField(choices=DEVICE_TYPE_CHOICES, default=WEB)
+    device_os = models.CharField(_('device os'), max_length=20, blank=True)
+    device_model = models.CharField(_('device model'), max_length=50, blank=True)
+    app_version = models.CharField(_('app version'), max_length=20, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'user_device'
+        verbose_name = _('device')
+        verbose_name_plural = _('devices')
+        unique_together = ('user', 'device_uuid')
+
+
+class Province(models.Model):
+    name = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
